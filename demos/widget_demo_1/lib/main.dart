e567e32e24b5e131a43bf0ff434fd941bcc6a12b
Joey Boerwinkel <joey@iconica.nl>
2022-09-12 15:08:00 +0200

    add widget demo

---
+++ demos/widget_demo_1/lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:widget_demo_1/title.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+    var state = titleKey.currentState;
+    if (state != null) {
+      state.tapText();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  final GlobalKey<AppTitleState> titleKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ButtonCounter(
+          counter: _counter,
+          titleKey: titleKey,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ButtonCounter extends StatelessWidget {
+  const ButtonCounter({
+    Key? key,
+    required this.titleKey,
+    required int counter,
+  })  : _counter = counter,
+        super(key: key);
+
+  final int _counter;
+  final GlobalKey<AppTitleState> titleKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        AppTitle(
+          text: 'You have pushed the button this many times:',
+          key: titleKey,
+        ),
+        Text(
+          '$_counter',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ],
+    );
+  }
+}
---
+++ demos/widget_demo_1/lib/title.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class AppTitle extends StatefulWidget {
+  final String text;
+
+  const AppTitle({
+    required this.text,
+    super.key,
+  });
+
+  @override
+  State<AppTitle> createState() => AppTitleState();
+}
+
+class AppTitleState extends State<AppTitle> {
+  bool isTapped = false;
+
+  @override
+  void initState() {
+    super.initState();
+    print(isTapped);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: tapText,
+      child: Text(
+        widget.text,
+        style: TextStyle(
+          color: Colors.blueGrey.shade800,
+          fontSize: 25,
+          fontWeight: isTapped ? FontWeight.bold : FontWeight.normal,
+        ),
+      ),
+    );
+  }
+
+  void tapText() {
+      setState(() {
+        isTapped = !isTapped;
+      });
+    }
+}
